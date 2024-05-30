0f9864d6e14980c05b62ae98c6770d532578fda2
LOGODERKING <Baleren.altintas@gmail.com>
2024-05-30 20:01:45 +0200

    second commit

---
+++ primzahl.py
@@ -0,0 +1,26 @@
+def ist_primzahl(n):
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+
+    i = 3
+    while i * i <= n:
+        if n % i:
+            i += 2
+        else:
+            return False
+    return True
+
+while True:
+    zahl = input("Geben Sie eine Zahl ein (oder 'q' zum Beenden): ")
+    if zahl.lower() == 'q':
+        break
+    else:
+        zahl = int(zahl)
+        if ist_primzahl(zahl):
+            print(str(zahl) + " ist eine Primzahl.")
+        else:
+            print(str(zahl) + " ist keine Primzahl.")
